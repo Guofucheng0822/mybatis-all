dbc9bf2ebaad4fe263fa8eb2759babe3b1dc3b31
““guofucheng <guofucheng@e6yun.com>
2020-10-10 10:58:43 +0800

    docs：修改日志打印

--- chapter3/src/main/java/com/pers/gfc/chapter3/typeHandle/MyStringTypeHandler.java
+++ chapter3/src/main/java/com/pers/gfc/chapter3/typeHandle/MyStringTypeHandler.java
@@ -1,14 +1,16 @@
 package com.pers.gfc.chapter3.typeHandle;
 
+import com.pers.gfc.chapter3.main.Chapter3Main;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * 自定义字符串类型处理的TypeHandler
@@ -17,32 +19,32 @@ import java.util.logging.Logger;
  * @date 2020/10/09
  */
 public class MyStringTypeHandler  extends BaseTypeHandler<String> {
-    private static final Logger logger=Logger.getLogger(MyStringTypeHandler.class.getName());
+    private static Logger logger = LogManager.getLogger(MyStringTypeHandler.class.getName());
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType)
             throws SQLException {
-        logger.log(Level.INFO,"---setNonNullParameter---自定义TypeHandler---");
+        logger.info("---setNonNullParameter---自定义TypeHandler---");
         ps.setString(i, parameter);
     }
 
     @Override
     public String getNullableResult(ResultSet rs, String columnName)
             throws SQLException {
-        logger.log(Level.INFO,"---getNullableResult---自定义TypeHandler---返回值:{}",rs.getString(columnName));
+        logger.info("---getNullableResult---自定义TypeHandler---返回值:{}",rs.getString(columnName));
         return rs.getString(columnName);
     }
 
     @Override
     public String getNullableResult(ResultSet rs, int columnIndex)
             throws SQLException {
-        logger.log(Level.INFO,"---getNullableResult---自定义TypeHandler--返回值:{}",rs.getString(columnIndex));
+        logger.info("---getNullableResult---自定义TypeHandler--返回值:{}",rs.getString(columnIndex));
         return rs.getString(columnIndex);
     }
 
     @Override
     public String getNullableResult(CallableStatement cs, int columnIndex)
             throws SQLException {
-        logger.log(Level.INFO,"---getNullableResult---自定义TypeHandler---返回值:{}",cs.getString(columnIndex));
+        logger.info("---getNullableResult---自定义TypeHandler---返回值:{}",cs.getString(columnIndex));
         return cs.getString(columnIndex);
     }
 }
